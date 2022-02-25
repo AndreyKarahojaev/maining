cbb077009e898bb7144fcdc8e15ee33807d9705d
a.karakhodzhaev <karahodschaev@yandex.ru>
2022-02-25 15:46:58 +0400

    garant-page-сделано

--- src/components/pages/Garant.vue
+++ src/components/pages/Garant.vue
@@ -4,11 +4,23 @@
     <div class="garant__content">
       <div class="content block-one">
         <span class="block-one--img">
-          <svg width="160" height="112" viewBox="0 0 160 112" fill="none" xmlns="http://www.w3.org/2000/svg">
-          <g opacity="0.3">
-          <path d="M49.5215 112C79.3941 112 99.3624 87.4033 99.3624 56C99.3624 24.2873 79.3941 0 49.841 0C20.1281 0 0 24.5967 0 56C0 87.7127 19.6488 112 49.5215 112ZM49.841 90.4972C34.9845 90.4972 25.24 75.8011 25.24 56C25.24 35.8895 34.8248 21.5028 49.5215 21.5028C64.2182 21.5028 74.1225 36.3536 74.1225 56C74.1225 75.9558 64.6974 90.4972 49.841 90.4972Z" fill="white"/>
-          <path d="M135.719 110.144H160V1.08287H143.227L111.118 10.3646L116.229 29.8563L135.719 25.2155V110.144Z" fill="white"/>
-          </g>
+          <svg
+            width="160"
+            height="112"
+            viewBox="0 0 160 112"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <g opacity="0.3">
+              <path
+                d="M49.5215 112C79.3941 112 99.3624 87.4033 99.3624 56C99.3624 24.2873 79.3941 0 49.841 0C20.1281 0 0 24.5967 0 56C0 87.7127 19.6488 112 49.5215 112ZM49.841 90.4972C34.9845 90.4972 25.24 75.8011 25.24 56C25.24 35.8895 34.8248 21.5028 49.5215 21.5028C64.2182 21.5028 74.1225 36.3536 74.1225 56C74.1225 75.9558 64.6974 90.4972 49.841 90.4972Z"
+                fill="white"
+              />
+              <path
+                d="M135.719 110.144H160V1.08287H143.227L111.118 10.3646L116.229 29.8563L135.719 25.2155V110.144Z"
+                fill="white"
+              />
+            </g>
           </svg>
         </span>
         <h4 class="content__title">
@@ -25,11 +37,23 @@
       </div>
       <div class="content block-two">
         <span class="block-two--img">
-          <svg width="192" height="112" viewBox="0 0 192 112" fill="none" xmlns="http://www.w3.org/2000/svg">
-          <g opacity="0.3">
-          <path d="M48.245 112C77.3476 112 96.8013 87.4033 96.8013 56C96.8013 24.2873 77.3476 0 48.5563 0C19.6093 0 0 24.5967 0 56C0 87.7127 19.1424 112 48.245 112ZM48.5563 90.4972C34.0828 90.4972 24.5894 75.8011 24.5894 56C24.5894 35.8895 33.9271 21.5028 48.245 21.5028C62.5629 21.5028 72.2119 36.3536 72.2119 56C72.2119 75.9558 63.0298 90.4972 48.5563 90.4972Z" fill="white"/>
-          <path d="M111.384 110.144H192V89.5691H144.533L162.275 75.9558C181.573 61.105 190.911 52.1326 190.911 34.0331C190.911 13.4586 175.97 0.309388 153.404 0.309388C133.95 0.309388 123.523 8.19889 112.785 22.7403L129.593 36.1989C137.685 26.2983 143.444 21.6575 151.848 21.6575C160.407 21.6575 166.477 26.6077 166.477 35.7348C166.477 44.7072 161.185 50.5856 147.646 61.7238L111.384 91.2707V110.144Z" fill="white"/>
-          </g>
+          <svg
+            width="192"
+            height="112"
+            viewBox="0 0 192 112"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <g opacity="0.3">
+              <path
+                d="M48.245 112C77.3476 112 96.8013 87.4033 96.8013 56C96.8013 24.2873 77.3476 0 48.5563 0C19.6093 0 0 24.5967 0 56C0 87.7127 19.1424 112 48.245 112ZM48.5563 90.4972C34.0828 90.4972 24.5894 75.8011 24.5894 56C24.5894 35.8895 33.9271 21.5028 48.245 21.5028C62.5629 21.5028 72.2119 36.3536 72.2119 56C72.2119 75.9558 63.0298 90.4972 48.5563 90.4972Z"
+                fill="white"
+              />
+              <path
+                d="M111.384 110.144H192V89.5691H144.533L162.275 75.9558C181.573 61.105 190.911 52.1326 190.911 34.0331C190.911 13.4586 175.97 0.309388 153.404 0.309388C133.95 0.309388 123.523 8.19889 112.785 22.7403L129.593 36.1989C137.685 26.2983 143.444 21.6575 151.848 21.6575C160.407 21.6575 166.477 26.6077 166.477 35.7348C166.477 44.7072 161.185 50.5856 147.646 61.7238L111.384 91.2707V110.144Z"
+                fill="white"
+              />
+            </g>
           </svg>
         </span>
         <h4 class="content__title">Подтверждено публичным пулом pool.in</h4>
@@ -44,11 +68,23 @@
       </div>
       <div class="content block-three">
         <span class="block-three--img">
-          <svg width="192" height="112" viewBox="0 0 192 112" fill="none" xmlns="http://www.w3.org/2000/svg">
-          <g opacity="0.3">
-          <path d="M48.4019 112C77.5992 112 97.1161 87.4033 97.1161 56C97.1161 24.2873 77.5992 0 48.7142 0C19.673 0 0 24.5967 0 56C0 87.7127 19.2046 112 48.4019 112ZM48.7142 90.4972C34.1936 90.4972 24.6694 75.8011 24.6694 56C24.6694 35.8895 34.0375 21.5028 48.4019 21.5028C62.7664 21.5028 72.4468 36.3536 72.4468 56C72.4468 75.9558 63.2348 90.4972 48.7142 90.4972Z" fill="#75ACFE"/>
-          <path d="M152.654 112C177.011 112 192 96.9945 192 76.5746C192 56.1547 177.479 48.4199 162.647 45.9447L190.283 19.8011V1.85635H116.431V22.2762H159.368L133.293 48.2652L137.04 63.4254H147.033C160.461 63.4254 168.424 68.3757 168.424 77.1934C168.424 85.547 162.022 90.6519 152.966 90.6519C141.881 90.6519 134.074 86.011 126.579 77.6575L110.029 93.2818C119.397 104.265 132.981 112 152.654 112Z" fill="#75ACFE"/>
-          </g>
+          <svg
+            width="192"
+            height="112"
+            viewBox="0 0 192 112"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <g opacity="0.3">
+              <path
+                d="M48.4019 112C77.5992 112 97.1161 87.4033 97.1161 56C97.1161 24.2873 77.5992 0 48.7142 0C19.673 0 0 24.5967 0 56C0 87.7127 19.2046 112 48.4019 112ZM48.7142 90.4972C34.1936 90.4972 24.6694 75.8011 24.6694 56C24.6694 35.8895 34.0375 21.5028 48.4019 21.5028C62.7664 21.5028 72.4468 36.3536 72.4468 56C72.4468 75.9558 63.2348 90.4972 48.7142 90.4972Z"
+                fill="#75ACFE"
+              />
+              <path
+                d="M152.654 112C177.011 112 192 96.9945 192 76.5746C192 56.1547 177.479 48.4199 162.647 45.9447L190.283 19.8011V1.85635H116.431V22.2762H159.368L133.293 48.2652L137.04 63.4254H147.033C160.461 63.4254 168.424 68.3757 168.424 77.1934C168.424 85.547 162.022 90.6519 152.966 90.6519C141.881 90.6519 134.074 86.011 126.579 77.6575L110.029 93.2818C119.397 104.265 132.981 112 152.654 112Z"
+                fill="#75ACFE"
+              />
+            </g>
           </svg>
         </span>
         <h4 class="content__title">Никаких скрытых действий</h4>
@@ -69,7 +105,8 @@
         alt="Пока картинкой, т.к нет данных"
       />
       <p class="img-block__text">
-        Наши возможности: <a href="https://www.poolin.com" class="img-block__link"
+        Наши возможности:
+        <a href="https://www.poolin.com" class="img-block__link"
           >https://www.poolin.com/my/9053220/b...</a
         >
       </p>
@@ -143,7 +180,6 @@ export default {};
   bottom: 5%;
 }
 
-
 .garant__content {
   width: 100%;
   max-width: 1408px;
@@ -176,7 +212,6 @@ export default {};
   line-height: $font-line140;
   text-align: left;
   margin: 0;
-  
 }
 .content__text {
   font-family: $font-family;
@@ -261,6 +296,83 @@ export default {};
   left: 34%;
   top: 50%;
   border-radius:8px ;
+}
+
+@media only screen and (min-device-width: 320px) and (max-device-width: 900px) {
+
+  .garant {
+    display: flex;
+    flex-direction: column;
+    flex-wrap: wrap;
+  }
+  // .garant-title {
+    
+  // }
+  .garant__content {
+    display: flex;
+    flex-direction: column;
+    flex-wrap: wrap;
+    padding: 32px;
+    width: 100%;
+    height: 100%;
+  }
+  .content {
+    display: flex;
+    flex-direction: column;
+    max-width: 328px;
+    max-height: 296px;
+    margin-bottom: 50px
+;
+  }
+  .content__title {
+    font-family: $font-family;
+    font-weight: $weight-bold;
+    font-size: $font-Text_18;
+    line-height: $font-line140;
+    text-align: left;
+    margin: 0;
+  }
+  .content__text {
+    font-family: $font-family;
+    font-size: $font-Text_14;
+    line-height: $font-line140;
+  }
+
+  .garant__img-block {
+    display: flex;
+    flex-direction: column;
+    width: 330px;
+    height: 160px;
+    margin: 0 auto;
+  }
+
+  .img-block__text {
+    font-size: $font-Text_18;
+    line-height: $font-line140;
+    text-align: center;
+    padding-bottom: 16px;
+
+    &::before {
+        position: absolute;
+        content: '';
+        display: block;
+        width: 32px;
+        height: 32px;
+        background: url('../../assets/img/icon/ic_atention.svg');
+        background-repeat: no-repeat;
+        background-size: contain;
+        right: 74%;
+        bottom: 52%;
+      }
+  }
+
+  .img-block__link {
+    text-decoration: underline;
+    color: $other-blue;
+    font-size: $font-Text_14;
+    line-height: 10%;
+    max-width: 312px;
+  }
 }
 
 </style>
